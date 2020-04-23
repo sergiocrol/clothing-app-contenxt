ddfefe48cae0b7b0a5bac52f1c848ba5a70edb81
Sergio Cordero <sergio.crol@gmail.com>
2020-04-24 00:25:32 +0200

    Change redux and implement Context

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+  const publishableKey = 'pk_test_bb9Qcf0uB2eGE4G99lIoflkA';
 
   const onToken = token => {
     console.log(token);
